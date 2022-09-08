333c48d1500826c880fcf05f8e711b41f9716a6a
aswick <aswickjothi27@gmail.com>
2022-09-08 12:06:37 +0530

    dddd

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,41 +1,67 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-import users from '../model/users.js';
+import users from "../model/users.js";
 
-router.post("/register",async(req,res)=>{
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  // console.log("=======",req.body);
+  const data = new users(req.body);
 
-    const { username,email, password } = req.body;
-    // console.log("=======",req.body);
-    const data = new users(req.body)
+  console.log("====================================");
+  console.log(data);
+  console.log("====================================");
 
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
+  let user = await users.findOne({ email });
 
-    let user = await users.findOne({email})
+  console.log("============", user?.email);
+  try {
+    if (data.email !== user?.email) {
+      await data.save();
+      res.status(200).json({
+        message: "user added successfully",
+      });
+    } else {
+      res.status(400).json({
+        messgae: "Email Already Registered",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "User Insert Failed",
+    });
+  }
+});
 
-    console.log("============",user?.email);
-    try{
-        if(data.email !== user?.email){
-        await data.save()
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const data = new users(req.body);
+  console.log("=====", data);
+
+  let emailID = await users.findOne({ email });
+  try {
+    if (emailID.email !== data.email) {
+      res.status(200).json({
+        message: "Email Not Found Please Register",
+      });
+    } else {
+      if (emailID.password === data.password) {
         res.status(200).json({
-            message:"user added successfully"
-        })}
-        else{
-            res.status(400).json({
-                messgae:"Email Already Registered"
-            })
-        }
+          message: "User Login Success",
+        });
+      } else {
+        res.status(200).json({
+          message: "Incorrect Password",
+        });
+      }
     }
-    catch(err){
-        res.status(500).json({
-            message:"User Insert Failed"
-        })
-    }
-})
-
+  } catch (err) {
+    res.status(500).json({
+      message: "Login Failed",
+    });
+  }
+});
 // router.get("/getuser",async(req,res)=>{
 //     const {id} = req.body
 //     try{
