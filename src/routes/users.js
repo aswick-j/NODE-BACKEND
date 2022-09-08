10b78e1c3d4d7279d79bcca49f7a34b98e5fa047
aswick <aswickjothi27@gmail.com>
2022-09-08 11:52:21 +0530

    rrrr

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,15 +4,30 @@ const router = express.Router();
 
 import users from '../model/users.js';
 
-router.post("/adduser",async(req,res)=>{
-    console.log("=======",req.body);
+router.post("/register",async(req,res)=>{
+
+    const { username,email, password } = req.body;
+    // console.log("=======",req.body);
     const data = new users(req.body)
-    console.log("============",data);
+
+    console.log('====================================');
+    console.log(data);
+    console.log('====================================');
+
+    let user = await users.findOne({email})
+
+    console.log("============",user?.email);
     try{
+        if(data.email !== user?.email){
         await data.save()
         res.status(200).json({
             message:"user added successfully"
-        })
+        })}
+        else{
+            res.status(400).json({
+                messgae:"Email Already Registered"
+            })
+        }
     }
     catch(err){
         res.status(500).json({
@@ -21,21 +36,21 @@ router.post("/adduser",async(req,res)=>{
     }
 })
 
-router.get("/getuser",async(req,res)=>{
-    const {id} = req.body
-    try{
-         await users.find()
+// router.get("/getuser",async(req,res)=>{
+//     const {id} = req.body
+//     try{
+//          await users.find()
 
-        res.status(200).json({
-            message:"user found successfully",
-            data
-        })
-    }
-    catch(err){
-        res.status(500).json({
-            message:"User fetch Failed"
-        })
-    }
+//         res.status(200).json({
+//             message:"user found successfully",
+//             data
+//         })
+//     }
+//     catch(err){
+//         res.status(500).json({
+//             message:"User fetch Failed"
+//         })
+//     }
 
-})
+// })
 export default router;
