5115d900b0f7868dfd6f3e376bac5dbf1c77d4bb
aswick <aswickjothi27@gmail.com>
2022-09-09 16:59:52 +0530

    ed

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,8 @@
 import express from "express";
 
-import { RegisterController,LoginController } from "../controllers/users.js";
-
 const router = express.Router();
 
-import users from "../model/users.js";
+import { RegisterController,LoginController } from "../controllers/users.js";
 
 router.post("/register", RegisterController);
 
