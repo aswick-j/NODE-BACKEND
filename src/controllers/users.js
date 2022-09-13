73dea9c675036dcf67f467beafb701c123d5d779
aswick <aswickjothi27@gmail.com>
2022-09-13 15:43:27 +0530

    JWT

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,8 +2,11 @@ import users from "../model/users.js";
 
 import bcrypt from "bcryptjs";
 
-export const RegisterController = async (req, res) => {
+import fs from "fs";
 
+import jwt from "jsonwebtoken";
+
+export const RegisterController = async (req, res) => {
   const { username, email, password } = req.body;
 
   const data = new users(req.body);
@@ -31,38 +34,42 @@ export const RegisterController = async (req, res) => {
 };
 
 export const LoginController = async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  const data = new users(req.body);
 
-    const data = new users(req.body);
+  let emailID = await users.findOne({ email });
 
-    let emailID = await users.findOne({ email });
+  const decryptpass = await bcrypt.compare(data.password, emailID.password);
 
+  try {
+    if (emailID.email !== data.email) {
+      res.status(400).json({
+        status: 400,
+        message: "Email Not Found Please Register",
+      });
+    } else {
+      if (decryptpass === true) {
+        const privateKey = "aasfgfgjk%hdnn9Onne";
 
-    const decryptpass =await bcrypt.compare( data.password,emailID.password )
-    
-    try {
-      if (emailID.email !== data.email) {
-        res.status(400).json({
-          status:400,
-          message: "Email Not Found Please Register",
+        const token = jwt.sign({ emailID }, privateKey,{});
+        console.log(token);
+
+        res.status(200).json({
+          status: 200,
+          message: "User Login Success",
+          token: token,
         });
       } else {
-        if (decryptpass === true) {
-          res.status(200).json({
-              status:200,
-            message: "User Login Success",
-          });
-        } else {
-          res.status(400).json({
-              status:400,
-            message: "Incorrect Password",
-          });
-        }
+        res.status(400).json({
+          status: 400,
+          message: "Incorrect Password",
+        });
       }
-    } catch (err) {
-      res.status(500).json({
-        message: "Login Failed",
-      });
     }
+  } catch (err) {
+    res.status(500).json({
+      message: "Login Failed",
+    });
   }
+};
